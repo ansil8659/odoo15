3710058271f5d8bcf3cebbe0e60a04146f7b059d
ansil8659 <80586067+ansil8659@users.noreply.github.com>
2022-12-19 17:31:22 +0530

    Add files via upload

---
+++ stock_availability_website/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': "Website Stock Availability",
+    'version': '15.01.0.0   ',
+    'depends': ['base', 'mail', 'account', 'website', 'sale_management'],
+    'author': "David Dazzil",
+    'category': 'Category',
+    'description': "show stock availability in website",
+    'application': True,
+
+    'data': [
+        'view/stock.xml',
+    ]
+}
